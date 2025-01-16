acda2cf4d221b9af14b9821c3cf8f756ab8c36ed
Patrick Böhme <pa.boe90@gmail.com>
2025-01-16 13:10:55 +0100

    removed unmaintained packages , intel --> modesetting

--- nixos/core/hardware/gpu/intel.nix
+++ nixos/core/hardware/gpu/intel.nix
@@ -2,13 +2,12 @@
 { config, pkgs, systemConfig, ... }:
 
 {
-  services.xserver.videoDrivers = [ "intel" ]; # Use the Intel driver for integrated graphics
+  services.xserver.videoDrivers = [ "modesetting" ]; # Use the Intel driver for integrated graphics
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
       vaapiIntel   # Intel VA-API driver for video acceleration.
       intel-media-sdk
-      xorg.xf86videointel
     ];
   };
 
