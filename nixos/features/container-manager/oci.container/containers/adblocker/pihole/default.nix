d33c30bbff8ae715bd8c18322ed3dbf330e830a7
Patrick Böhme <pa.boe90@gmail.com>
2025-01-16 12:56:40 +0100

    Add default.nix

---
+++ nixos/features/container-manager/oci.container/containers/adblocker/pihole/default.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.services.pihole;
+  containerVars = import ./vars.nix { inherit lib config; };
+  piholeConfig = import ./pihole.nix { inherit lib pkgs config containerVars; };
+in {
+  imports = [
+    ./config.nix
+    ./volumes.nix
+    ./container.nix
+    ./vars.nix
+  ];
+
+  services.pihole = {
+    enable = true;
+    subdomain = "pihole";
+    domain = "example.com";
+    
+  };
+}
