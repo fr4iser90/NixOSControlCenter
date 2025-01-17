6bc376b0a37448310261ee475a913a6d56aa4cad
Patrick Böhme <pa.boe90@gmail.com>
2025-01-17 20:05:03 +0100

    Update default.nix

--- nixos/custom/default.nix
+++ nixos/custom/default.nix
@@ -1,7 +1,15 @@
 { config, lib, pkgs, ... }:
 
+let
+  currentDir = toString ./.;
+  # Load all .nix files in current directory
+  nixFiles = builtins.attrNames (lib.filterAttrs 
+    (_: v: lib.hasSuffix ".nix" v) 
+    (builtins.readDir currentDir));
+  
+  # Create full import paths
+  imports = map (file: currentDir + "/${file}") nixFiles;
+in
 {
-  imports = [
-    ./kde-connect.nix
-  ];
+  imports = if nixFiles == [] then [] else imports;
 }
