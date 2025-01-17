8b5946fd9acdd2ab1a7f54f81675cb5e4eea8d96
Patrick Böhme <pa.boe90@gmail.com>
2025-01-17 20:05:03 +0100

    Update default.nix

--- nixos/features/homelab-manager/default.nix
+++ nixos/features/homelab-manager/default.nix
@@ -1,12 +1,25 @@
-# homelab-manager/default.nix
 { config, lib, pkgs, systemConfig, ... }:
 
-{
-  imports = [
-    ./homelab-fetch.nix
+let
+  # Finde alle Benutzer mit virtualization Rolle
+  virtUsers = lib.filterAttrs 
+    (name: user: user.role == "virtualization") 
+    systemConfig.users;
+
+  # Prüfe ob wir Virtualisierungsbenutzer haben
+  hasVirtUsers = (lib.length (lib.attrNames virtUsers)) > 0;
+
+  # Hole den ersten Virtualisierungsbenutzer, falls vorhanden
+  virtUser = lib.head (lib.attrNames virtUsers);
+
+in {
+
+
+  imports = if hasVirtUsers then [
     ./homelab-create.nix
-#    ./homelab-update.nix
-#    ./homelab-delete.nix
-#    ./homelab-list.nix
-  ];
+    ./homelab-fetch.nix
+    # ./homelab-update.nix
+    # ./homelab-delete.nix
+    # ./homelab-status.nix
+  ] else [];
 }
