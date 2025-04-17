505ef117016531c88fb193e58dbfa277aa4553a9
Patrick Böhme <pa.boe90@gmail.com>
2025-04-17 22:54:55 +0200

    chore: remove git-credential-manager configuration from default.nix
    - Removed git-credential-manager from system packages.
    - Deleted related git configuration for credential management.

--- nixos/packages/modules/development/default.nix
+++ nixos/packages/modules/development/default.nix
@@ -5,13 +5,6 @@
     vscode
     code-cursor
     git
-    git-credential-manager
     delta
   ];
-  programs.git = {
-    enable = true;
-    config = {
-      credential.helper = "manager";
-    };
-  };
 }
